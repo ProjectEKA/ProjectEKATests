d1913492d91986117737d0ea285d20876e4170e7
shridhar kalagi <shridharkalagi@gmail.com>
2020-05-28 16:32:11 +0530

    add missing file

---
+++ src/test/java/Tests/MobileAppTests/Utils/Patient.java
@@ -0,0 +1,25 @@
+package Tests.MobileAppTests.Utils;
+
+public class Patient {
+    public String patientName;
+    public String mobile;
+    public String gender;
+
+    public Patient(String patientName, String mobile, String gender) {
+        this.patientName = patientName;
+        this.mobile = mobile;
+        this.gender = gender;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+}
