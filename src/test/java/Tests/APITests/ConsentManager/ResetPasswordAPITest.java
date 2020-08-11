255c4f8721f44b83b87eeb9a6b931d6f7c3b17d6
shridhar kalagi <shridharkalagi@gmail.com>
2020-08-11 20:20:46 +0530

    add more log

--- src/test/java/Tests/APITests/ConsentManager/ResetPasswordAPITest.java
+++ src/test/java/Tests/APITests/ConsentManager/ResetPasswordAPITest.java
@@ -12,7 +12,6 @@ import org.testng.annotations.Test;
 
 public class ResetPasswordAPITest {
 
-    private static String authToken;
     private static String sessionId;
     private static String otpAuthToken;
     RequestSpecification request;
@@ -24,46 +23,33 @@ public class ResetPasswordAPITest {
         request = RestAssured.given();
     }
 
-    @Test(priority = 0)
+    @Test
     public void generateOTPAPI() {
-        System.out.println("generateOTPAPI");
         //generate otp for enter phone #
         request.header("Content-Type", "application/json");
         request.body(new ResetPassword().getGenerateOTPRequestBody());
         Response generateOTPResponse = request.post("/patients/generateotp");
-
         Assert.assertEquals(generateOTPResponse.getStatusCode(), 201);
         sessionId = generateOTPResponse.jsonPath().getString("sessionId");
-        System.out.println(sessionId + " Seession ID");
     }
 
-    @Test(dependsOnMethods = "generateOTPAPI", priority = 1)
+    @Test(dependsOnMethods = "generateOTPAPI")
     public void verifyOTPAPI() {
-        System.out.println("verifyOTPAPI");
         //verify the otp
         request.header("Content-Type", "application/json");
-        System.out.println(sessionId + " Seession ID next");
         request.body(new ResetPassword().getVerifyOTPRequestBody(sessionId));
         Response verifyOTPResponse = request.post("/patients/verifyotp");
-
         Assert.assertEquals(verifyOTPResponse.getStatusCode(), 200);
         otpAuthToken = verifyOTPResponse.jsonPath().getString("temporaryToken");
-        System.out.println(otpAuthToken + "1234567890");
     }
 
-    @Test(dependsOnMethods = "verifyOTPAPI", priority = 2)
+    @Test(dependsOnMethods = "verifyOTPAPI")
     public void resetPasswordAPI() {
-
         //reset-password after otp confirmation
-
         request.header("Content-Type", "application/json");
-        System.out.println(otpAuthToken + "0987654321");
         request.header("Authorization", otpAuthToken);
         request.body(new ResetPassword().getResetPasswordRequestBody());
-        System.out.println(new ResetPassword().getResetPasswordRequestBody() + "ppppp");
         Response resetPasswordResponse = request.put("/patients/profile/reset-password");
-
-        System.out.println(resetPasswordResponse.getBody().asString() + "mmmmmm");
 
         Assert.assertEquals(resetPasswordResponse.getStatusCode(), 200);
     }
